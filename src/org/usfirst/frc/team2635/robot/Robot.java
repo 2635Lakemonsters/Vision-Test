7ebe9ba604e2f8d6a4161d26e6b5559717a505fa
2635Lakemonsters <lakemonsters.software.team@gmail.com>
2016-02-08 21:02:59 -0800

    Got an angle returned using a color measurment.

--- src/org/usfirst/frc/team2635/robot/Robot.java
+++ src/org/usfirst/frc/team2635/robot/Robot.java
@@ -38,8 +38,8 @@ public class Robot extends IterativeRobot {
      * used for any initialization code.
      */
 	public class ParticleReport implements Comparator<ParticleReport>, Comparable<ParticleReport>{
-		double PercentAreaToImageArea;
-		double Area;
+		double HorizontalSegments;
+		double VerticalSegments;
 		double BoundingRectLeft;
 		double BoundingRectTop;
 		double BoundingRectWidth;
@@ -47,15 +47,17 @@ public class Robot extends IterativeRobot {
 		
 		public int compareTo(ParticleReport r)
 		{
-			return (int)(r.Area - this.Area);
+			return (int)(r.VerticalSegments - this.VerticalSegments);
 		}
 		
 		public int compare(ParticleReport r1, ParticleReport r2)
 		{
-			return (int)(r1.Area - r2.Area);
+			return (int)(r1.VerticalSegments - r2.VerticalSegments);
 		}
 	};
-	
+	SendableChooser cameraView;
+	String BINARY = "binary";
+	String COLOR = "color";
     int session;
     Image colorFrame;
     Image binaryFrame;
@@ -67,7 +69,7 @@ public class Robot extends IterativeRobot {
 	double MAX_RECT_WIDTH = 200.0;
 	double MIN_RECT_HEIGHT = 0.0;
 	double MAX_RECT_HEIGHT = 200.0;
-	
+	int CAMERA_RESOLUTION_X = 640;
 	//I have no clue what a scale range is. Calibrate it through dashboard.
 	NIVision.RangeFloat SCALE_RANGE = new NIVision.RangeFloat(0.0, 200.0);
 	int MIN_MATCH_SCORE = 0;
@@ -75,7 +77,7 @@ public class Robot extends IterativeRobot {
 	double AREA_MINIMUM = 0.5; //Default Area minimum for particle as a percentage of total image area
 	double LONG_RATIO = 20.0/12.0; //Retro long side = 20 / Retro height = 12 
 	double SCORE_MIN = 75.0;  //Minimum score to be considered a match
-	double VIEW_ANGLE = 60; //View angle fo camera, set to Axis m1011 by default, 64 for m1013, 51.7 for 206, 52 for HD3000 square, 60 for HD3000 640x480
+	double VIEW_ANGLE = 64.0; //View angle fo camera, set to Axis m1011 by default, 64 for m1013, 51.7 for 206, 52 for HD3000 square, 60 for HD3000 640x480
 	NIVision.ParticleFilterCriteria2 criteria[] = new NIVision.ParticleFilterCriteria2[1];
 	NIVision.ParticleFilterOptions2 filterOptions = new NIVision.ParticleFilterOptions2(0,0,1,1);
 	NIVision.RangeFloat rectAngleRanges[] = {new NIVision.RangeFloat(60.0, 110.0)};
@@ -98,7 +100,10 @@ public class Robot extends IterativeRobot {
 		SmartDashboard.putNumber("Retro sat max", RETRO_SAT_RANGE.maxValue);
 		SmartDashboard.putNumber("Retro val min", RETRO_VAL_RANGE.minValue);
 		SmartDashboard.putNumber("Retro val max", RETRO_VAL_RANGE.maxValue);
-		
+		cameraView = new SendableChooser();
+		cameraView.addDefault("Color", COLOR);
+		cameraView.addObject("Binary", BINARY);
+		SmartDashboard.putData("Camera View",cameraView);
 //		SmartDashboard.putNumber("Min Rect Width", MIN_RECT_WIDTH);
 //		SmartDashboard.putNumber("Max Rect Width", MAX_RECT_WIDTH);
 //		SmartDashboard.putNumber("Min Rect Height", MIN_RECT_HEIGHT);
@@ -124,7 +129,16 @@ public class Robot extends IterativeRobot {
     public void autonomousPeriodic() {
     
     }
-    
+    public double pixelCoordnateToAimingCoordnate(double pixelCoordnate, double resolution)
+    {
+    	//Shift pixelCoordnate to the right by half the resolution, effectively transforming the plane of the coordnate from 0 on the lefthand side to -resolution/2 on the lefthand side
+    	//Divide by half the resolution to get the ratio between the coordnate position and its distance to 0.
+    	return (pixelCoordnate - resolution / 2.0) / (resolution / 2.0);
+    }
+    public double aimingCoordnateToAngle(double aimingCoordnate, double viewingAngle)
+    {
+    	return aimingCoordnate * viewingAngle/2.0;
+    }
     public void teleopPeriodic() {
     	//Grab a frame from the camera
         NIVision.IMAQdxGrab(session, colorFrame, 1);				
@@ -146,10 +160,13 @@ public class Robot extends IterativeRobot {
         //Look at the color frame for colors that fit the range. Colors that fit the range will be transposed as a 1 to the binary frame.
 		NIVision.imaqColorThreshold(binaryFrame, colorFrame, 255, ColorMode.HSV, RETRO_HUE_RANGE, RETRO_SAT_RANGE, RETRO_VAL_RANGE);
 		//Send the binary image to the cameraserver
-
+		if(cameraView.getSelected() == BINARY)
+		{
+			CameraServer.getInstance().setImage(binaryFrame);
+		}
 
 		criteria[0] = new NIVision.ParticleFilterCriteria2(NIVision.MeasurementType.MT_AREA_BY_IMAGE_AREA, AREA_MINIMUM, 100.0, 0, 0);		
-
+		
         NIVision.imaqParticleFilter4(binaryFrame, binaryFrame, criteria, filterOptions, null);
 
         //        NIVision.RectangleDescriptor rectangleDescriptor = new NIVision.RectangleDescriptor(MIN_RECT_WIDTH, MAX_RECT_WIDTH, MIN_RECT_HEIGHT, MAX_RECT_HEIGHT);
@@ -189,8 +206,8 @@ public class Robot extends IterativeRobot {
 			for(int particleIndex = 0; particleIndex < numParticles; particleIndex++)
 			{
 				ParticleReport par = new ParticleReport();
-				par.PercentAreaToImageArea = NIVision.imaqMeasureParticle(binaryFrame, particleIndex, 0, NIVision.MeasurementType.MT_AREA_BY_IMAGE_AREA);
-				par.Area = NIVision.imaqMeasureParticle(binaryFrame, particleIndex, 0, NIVision.MeasurementType.MT_AREA);
+				par.HorizontalSegments = NIVision.imaqMeasureParticle(binaryFrame, particleIndex, 0, NIVision.MeasurementType.MT_NUMBER_OF_VERT_SEGMENTS);
+				par.VerticalSegments = NIVision.imaqMeasureParticle(binaryFrame, particleIndex, 0, NIVision.MeasurementType.MT_NUMBER_OF_VERT_SEGMENTS);
 				par.BoundingRectTop = NIVision.imaqMeasureParticle(binaryFrame, particleIndex, 0, NIVision.MeasurementType.MT_BOUNDING_RECT_TOP);
 				par.BoundingRectLeft = NIVision.imaqMeasureParticle(binaryFrame, particleIndex, 0, NIVision.MeasurementType.MT_BOUNDING_RECT_LEFT);
 				par.BoundingRectHeight = NIVision.imaqMeasureParticle(binaryFrame, particleIndex, 0, NIVision.MeasurementType.MT_BOUNDING_RECT_HEIGHT);
@@ -216,7 +233,15 @@ public class Robot extends IterativeRobot {
 		    NIVision.Rect particleRect = new NIVision.Rect((int)bestParticle.BoundingRectTop, (int)bestParticle.BoundingRectLeft, (int)bestParticle.BoundingRectHeight, (int)bestParticle.BoundingRectWidth);
 		    
 		    //NIVision.imaqOverlayRect(colorFrame, particleRect, new NIVision.RGBValue(0, 0, 0, 255), DrawMode.PAINT_VALUE, "a");//;(colorFrame, colorFrame, particleRect, DrawMode.DRAW_VALUE, ShapeMode.SHAPE_OVAL, 20);
-		    NIVision.imaqDrawShapeOnImage(colorFrame, colorFrame, particleRect, NIVision.DrawMode.DRAW_VALUE, ShapeMode.SHAPE_RECT, 50.0f);
+		    NIVision.imaqDrawShapeOnImage(colorFrame, colorFrame, particleRect, NIVision.DrawMode.DRAW_VALUE, ShapeMode.SHAPE_RECT, 0.0f);
+		    SmartDashboard.putNumber("Rect Top", bestParticle.BoundingRectTop);
+		    SmartDashboard.putNumber("Rect Left", bestParticle.BoundingRectLeft);
+		    SmartDashboard.putNumber("Rect Width", bestParticle.BoundingRectWidth);
+		    SmartDashboard.putNumber("Vertical Segments", bestParticle.VerticalSegments);
+		    SmartDashboard.putNumber("Horizontal Segments", bestParticle.HorizontalSegments);
+		    double bestParticleMidpoint = bestParticle.BoundingRectLeft + bestParticle.BoundingRectWidth/2.0;
+		    double bestParticleMidpointAimingCoordnates = pixelCoordnateToAimingCoordnate(bestParticleMidpoint, CAMERA_RESOLUTION_X);
+		    SmartDashboard.putNumber("Angle to target", aimingCoordnateToAngle(bestParticleMidpointAimingCoordnates, VIEW_ANGLE));
 		    
 		    //NIVision.imaqMergeOverlay(colorFrame, colorFrame, new NIVision.RGBValue[]{new NIVision.RGBValue(0,0,0,255)}, "a");
 		    //			NIVision.imaqOverlayText(
@@ -242,8 +267,10 @@ public class Robot extends IterativeRobot {
 //		else {
 //			SmartDashboard.putBoolean("IsTote", false);
 //		}
-        CameraServer.getInstance().setImage(colorFrame);
-		System.out.println("hi");
+        if(cameraView.getSelected() == COLOR)
+        {
+        	CameraServer.getInstance().setImage(colorFrame);
+        }
 
     }
     
